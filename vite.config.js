448717759abd31a401a85476fff94a0c81628569
Ame <deoxis9001@users.noreply.github.com>
2026-04-26 22:23:53 +0200

    ci: add build & release workflow + fix image base paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  base: '/tmcr-tracker/',
-  plugins: [vue()],
+  base: './',
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
